cc3430af1520d3b1a0d2700cd717aac02a6f9246
abob78 <artin.alavi@gmail.com>
2014-08-23 10:13:00 -0700

    Added recipes
    Again, I highly recomend putting this in main class file

--- main/java/com/noobieteam/fallacia/init/Recipes.java
+++ main/java/com/noobieteam/fallacia/init/Recipes.java
@@ -5,12 +5,31 @@ package com.noobieteam.fallacia.init;
  * Registers them..
  */
 public class Recipes {
-    // CREATE ALL RECIPES's INSCANCES IN HERE
-
-    /** Call the game register in this method
-     *
-     */
-    public static void init() {
-
+    @EventHandler
+    public void init(FMLInitializationEvent event) {
+    	GameRegistry.addRecipe(new ItemStack(fallaciaBlock), new Object[]{
+    		"y y",//Must be in the correct format: a 3x3 grid for the crafting table
+    		"xyx",
+    		"y y",
+    		'y', Blocks.obsidian, 'x', new ItemStack(Blocks.stained_glass, 1, 9) //dictates what x and y equal
+    		                                                                     //Use itemstack if you want to use a varation (such as a color of wool)
+    	});                                                                          //put in by Abob78
+    	GameRegistry.addRecipe(new ItemStack(magicalWood), new Object[]{
+    		"y y",
+    		"xyx",
+    		"y y",
+    		'y', Blocks.obsidian, 'x', new ItemStack(Blocks.stained_glass, 1, 9) //change the block accordingly
+    	});                                                                          //you can change it to with no itemstack if u just want a base block
+    	GameRegistry.addRecipe(new ItemStack(magicalLeaves), new Object[]{
+    		"y y",
+    		"xyx",
+    		"y y",
+    		'y', Blocks.obsidian, 'x', new ItemStack(Blocks.stained_glass, 1, 9) 
+    	});   
+    	GameRegistry.addRecipe(new ItemStack(magicalSapling), new Object[]{
+    		"y y",
+    		"xyx",
+    		"y y",
+    		'y', Blocks.obsidian, 'x', new ItemStack(Blocks.stained_glass, 1, 9) 
+    	});   
     }
-}
