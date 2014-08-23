c661f68e4a21b12dea25757482f20e06b81948c4
Daniel Travieso <danielgtravieso@gmail.com>
2014-08-23 14:36:52 -0300

    fixed some errors

--- main/java/com/noobieteam/fallacia/init/ModBlocks.java
+++ main/java/com/noobieteam/fallacia/init/ModBlocks.java
@@ -1,10 +1,15 @@
 package com.noobieteam.fallacia.init;
 
 
+import com.noobieteam.fallacia.block.FallaciaBlock;
 import com.noobieteam.fallacia.block.tree.MagicalLeaves;
 import com.noobieteam.fallacia.block.tree.MagicalSapling;
 import com.noobieteam.fallacia.block.tree.MagicalWood;
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 import cpw.mods.fml.common.registry.GameRegistry;
+import net.minecraft.block.Block;
+import net.minecraft.block.material.Material;
 
 /**
  * Main init mod initializers for blocks
@@ -12,7 +17,7 @@ import cpw.mods.fml.common.registry.GameRegistry;
  */
 public class ModBlocks {
     // CREATE ALL MOD BLOCK's INSTANCES IN HERE
-        public static Block fallaciaBlock = new FallaciaBlock();
+    public static Block fallaciaBlock = new FallaciaBlock(Material.rock);
         public static Block magicalWood = new MagicalWood();
         public static Block magicalSapling = new MagicalSapling();
         public static Block magicalLeaves = new MagicalLeaves();
@@ -20,7 +25,7 @@ public class ModBlocks {
     /**
      * Call all the GameRegistry.registerBlock in here..
      */
-    @EventHandler
+    @Mod.EventHandler
     public void preinit(FMLPreInitializationEvent event) {
         GameRegistry.registerBlock(fallaciaBlock, "fallaciaBlock");
         GameRegistry.registerBlock(magicalWood, "magicalWood");
