44b26ba5e57994e9bb0fc3e0f0fc8c904fcd9045
abob78 <artin.alavi@gmail.com>
2014-08-23 10:09:23 -0700

    Update Fallacia.java
    added recipes and blocks into the registry

--- main/java/com/noobieteam/fallacia/Fallacia.java
+++ main/java/com/noobieteam/fallacia/Fallacia.java
@@ -9,14 +9,25 @@ import cpw.mods.fml.common.event.FMLInitializationEvent;
 import cpw.mods.fml.common.event.FMLPostInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 import scala.tools.nsc.backend.icode.TypeKinds;
+import com.noobieteam.fallacia.block.FallaciaBlock;
+import com.noobieteam.fallacia.block.tree;
+import net.minecraft.block.Block;
 
 /**
- * Main mod Class, it handles preinit, init and post init
+ * Main mod Class
+ *
+ * public class Fallacia makes the variables for the blocks so they can be registered
+ * but block info and properites need ot be on the class files
+ * (put in by Abob78)
  */
-@Mod(modid = Reference.MOD_ID, name = Reference.MOD_NAME, version = Reference.VERSION,
-        guiFactory = Reference.GUI_FACTORY_CLASS)
+@Mod(modid = Reference.MOD_ID, name = Reference.MOD_NAME, version = Reference.VERSION, guiFactory = Reference.GUI_FACTORY_CLASS)
 public class Fallacia {
+        public static Block fallaciaBlock = new FallaciaBlock();
+        public static Block magicalWood = new MagicalWood();
+        public static Block magicalSapling = new MagicalSapling();
+        public static Block magicalLeaves = new MagicalLeaves();
 
+}
     /**
      * This registers a static instance of the mod's class. It is useful for
      * the proxy and gui stuff
@@ -34,20 +45,51 @@ public class Fallacia {
      * Pre initialization of stuff!
      * like Blocks, Items and Such
      * @param event
+     * 
+     * Registers Blocks to the game (put in by Abob78)
      */
-    @Mod.EventHandler
+    @EventHandler
     public void preinit(FMLPreInitializationEvent event) {
-        ModBlocks.init();
+        GameRegistry.registerBlock(fallaciaBlock, "Fallacia Block");
+        GameRegistry.registerBlock(magicalLeaves, "Magical Leaves");
+        GameRegistry.registerBlock(magicalWood, "Magical Wood");
+        GameRegistry.registerBlock(magicalSapling, "Magical Sapling");
     }
 
     /**
      * Initialization of stuff!
      * Like recipes and event handlers
      * @param event
+     * 
+     * Registers Recipes for the blocks
      */
-    @Mod.EventHandler
+    @EventHandler
     public void init(FMLInitializationEvent event) {
-
+    	GameRegistry.addRecipe(new ItemStack(fallaciaBlock), new Object[]{
+    		"y y",//Must be in the correct format: a 3x3 grid for the crafting table
+    		"xyx",
+    		"y y",
+    		'y', Blocks.obsidian, 'x', new ItemStack(Blocks.stained_glass, 1, 9) //dictates what x and y equal
+    		                                                                     //Use itemstack if you want to use a varation (such as a color of wool)
+    	});                                                                          //put in by Abob78
+    	GameRegistry.addRecipe(new ItemStack(magicalWood), new Object[]{
+    		"y y",
+    		"xyx",
+    		"y y",
+    		'y', Blocks.obsidian, 'x', new ItemStack(Blocks.stained_glass, 1, 9) //change the block accordingly
+    	});                                                                          //you can change it to with no itemstack if u just want a base block
+    	GameRegistry.addRecipe(new ItemStack(magicalLeaves), new Object[]{
+    		"y y",
+    		"xyx",
+    		"y y",
+    		'y', Blocks.obsidian, 'x', new ItemStack(Blocks.stained_glass, 1, 9) 
+    	});   
+    	GameRegistry.addRecipe(new ItemStack(magicalSapling), new Object[]{
+    		"y y",
+    		"xyx",
+    		"y y",
+    		'y', Blocks.obsidian, 'x', new ItemStack(Blocks.stained_glass, 1, 9) 
+    	});   
     }
 
     /**
@@ -55,7 +97,7 @@ public class Fallacia {
      * like loggin and such (dont really know =P)
      * @param event
      */
-    @Mod.EventHandler
+    @EventHandler
     public void postinit(FMLPostInitializationEvent event) {
 
     }
