f229227bd847c5413793e568130b4e005c1aea11
abob78 <artin.alavi@gmail.com>
2014-08-23 10:11:40 -0700

    removed recipes and put it into recipes.java
    I highly recon=mend putting in the main class since its important

--- main/java/com/noobieteam/fallacia/Fallacia.java
+++ main/java/com/noobieteam/fallacia/Fallacia.java
@@ -65,32 +65,7 @@ public class Fallacia {
      */
     @EventHandler
     public void init(FMLInitializationEvent event) {
-    	GameRegistry.addRecipe(new ItemStack(fallaciaBlock), new Object[]{
-    		"y y",//Must be in the correct format: a 3x3 grid for the crafting table
-    		"xyx",
-    		"y y",
-    		'y', Blocks.obsidian, 'x', new ItemStack(Blocks.stained_glass, 1, 9) //dictates what x and y equal
-    		                                                                     //Use itemstack if you want to use a varation (such as a color of wool)
-    	});                                                                          //put in by Abob78
-    	GameRegistry.addRecipe(new ItemStack(magicalWood), new Object[]{
-    		"y y",
-    		"xyx",
-    		"y y",
-    		'y', Blocks.obsidian, 'x', new ItemStack(Blocks.stained_glass, 1, 9) //change the block accordingly
-    	});                                                                          //you can change it to with no itemstack if u just want a base block
-    	GameRegistry.addRecipe(new ItemStack(magicalLeaves), new Object[]{
-    		"y y",
-    		"xyx",
-    		"y y",
-    		'y', Blocks.obsidian, 'x', new ItemStack(Blocks.stained_glass, 1, 9) 
-    	});   
-    	GameRegistry.addRecipe(new ItemStack(magicalSapling), new Object[]{
-    		"y y",
-    		"xyx",
-    		"y y",
-    		'y', Blocks.obsidian, 'x', new ItemStack(Blocks.stained_glass, 1, 9) 
-    	});   
-    }
+
 
     /**
      * Post initialization of stuff
