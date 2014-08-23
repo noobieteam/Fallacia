21241facb104b6c1d06429fe156529b4c3859611
Daniel Travieso <danielgtravieso@gmail.com>
2014-08-23 01:13:16 -0300

    created the magical wood, leaves and sapling... There are still some things to be done to the sappling, but im tired. =(

--- main/java/com/noobieteam/fallacia/init/ModBlocks.java
+++ main/java/com/noobieteam/fallacia/init/ModBlocks.java
@@ -1,17 +1,27 @@
 package com.noobieteam.fallacia.init;
 
 
+import com.noobieteam.fallacia.block.tree.MagicalLeaves;
+import com.noobieteam.fallacia.block.tree.MagicalSapling;
+import com.noobieteam.fallacia.block.tree.MagicalWood;
+import cpw.mods.fml.common.registry.GameRegistry;
+
 /**
  * Main init mod initializers for blocks
  * Register the Blocks on GameRegistry
  */
 public class ModBlocks {
     // CREATE ALL MOD BLOCK's INSTANCES IN HERE
+    private static MagicalWood magicalWood = new MagicalWood();
+    private static MagicalLeaves magicalLeaves = new MagicalLeaves();
+    private static MagicalSapling magicalSapling = new MagicalSapling();
 
     /**
      * Call all the GameRegistry.registerBlock in here..
      */
     public static void init() {
-
+        GameRegistry.registerBlock(magicalWood, "magicalWood");
+        GameRegistry.registerBlock(magicalLeaves, "magicalLeaves");
+        GameRegistry.registerBlock(magicalSapling, "magicalSapling");
     }
 }
