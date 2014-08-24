8d56d052c4b64df22006546b2950abcef8c5699a
abob78 <artin.alavi@gmail.com>
2014-08-24 15:02:47 -0700

    Update Reference.java

--- main/java/com/noobieteam/fallacia/reference/Reference.java
+++ main/java/com/noobieteam/fallacia/reference/Reference.java
@@ -3,10 +3,11 @@ package com.noobieteam.fallacia.reference;
 /**
  * Main reference class, it is the place where we put text that will be repeated on the code
  */
+@Mod
 public class Reference {
     public static final String MOD_ID = "fallacia";
     public static final String MOD_NAME = "Fallacia";
-    public static final String VERSION = "1.7.10-1.0";
+    public static final String VERSION = "1.0";
     public static final String CLIENT_PROXY_CLASS = "com.noobieteam.fallacia.proxy.ClientProxy";
     public static final String SERVER_PROXY_CLASS = "com.noobieteam.fallacia.proxy.ServerProxy";
     public static final String GUI_FACTORY_CLASS = "com.noobieteam.fallacia.client.gui.GuiFactory";
