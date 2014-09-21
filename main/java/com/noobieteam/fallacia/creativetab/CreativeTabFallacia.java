03f893b7588a9f53774afa2b63ab718e24862f6e
abob78 <artin.alavi@gmail.com>
2014-09-20 17:58:30 -0700

    Update CreativeTabFallacia.java

--- main/java/com/noobieteam/fallacia/creativetab/CreativeTabFallacia.java
+++ main/java/com/noobieteam/fallacia/creativetab/CreativeTabFallacia.java
@@ -5,6 +5,7 @@ import com.noobieteam.fallacia.block.FallaciaBlock;
 import com.noobieteam.fallacia.init.ModBlocks;
 import com.noobieteam.fallacia.init.ModItems;
 import com.noobieteam.fallacia.item.FallaciaItem;
+import com.noobieteam.fallacia.item;
 import com.noobieteam.fallacia.reference.Reference;
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.init.Blocks;
@@ -18,7 +19,7 @@ public class CreativeTabFallacia {
     public static final CreativeTabs tabFallacia = new CreativeTabs(Reference.MOD_ID.toLowerCase()) {
         @Override
         public Item getTabIconItem() {
-            return Items.FallaciaItem;
+            return FallaciaItem.FallaciaItem; //custom item
         }
     };
 }
